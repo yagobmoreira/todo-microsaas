e316ea220234a8b4204cc1b5662027573099dac0
Yago Moreira <yagomoreira@gmail.com>
2024-08-23 13:02:06 -0300

    feat: add prisma, nodemailer and next-auth

--- src/app/auth/_components/auth-form.tsx
+++ src/app/auth/_components/auth-form.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
+import { signIn } from "next-auth/react"
 
 type FormData = {
   email: string;
@@ -40,7 +41,7 @@ export function AuthForm() {
     setSuccess(false)
 
     try {
-      await sendMagicLink(data.email)
+      await signIn("nodemailer", {redirect: false, email: data.email})
       setSuccess(true)
     } catch (err) {
       console.error('An error occurred:', err)
--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,7 +1,24 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import Nodemailer from "next-auth/providers/nodemailer"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "../database"
  
 export const { auth, handlers } = NextAuth({
-  providers: [GitHub, Google],
+  providers: [
+    GitHub,
+    Google,
+    Nodemailer({
+    server: {
+      host: process.env.EMAIL_SERVER_HOST,
+      port: process.env.EMAIL_SERVER_PORT,
+      auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
+      },
+    },
+    from: process.env.EMAIL_FROM,
+  }),],
+  adapter: PrismaAdapter(prisma)
 })
